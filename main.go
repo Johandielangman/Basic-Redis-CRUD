5194b4d7342c18acd07250bef9a0a43b24b177a5
Johan Hanekom <jghanekom2@gmail.com>
2025-07-04 22:48:07 +0200

    feat: add redis connection, sigint and model

--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"chi2/application"
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	fmt.Println("Starting Server on port 3000")
 
 	app := application.New()
-	err := app.Start(context.TODO())
+
+	// ====> ROOT LEVEL CONTEXT
+	// If we cancel the root, all the children ones will be cancelled as well.
+	// We define a sigint
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("Failed to start server:", err)
 	}
